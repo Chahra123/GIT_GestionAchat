116609ed50d33ebcb712356a6f89341c6e05c9c5
Chahrazed Ben Azaiez <chahrazed.benazaiez@neoxam.com>
2023-03-31 08:47:13 +0000

    add ProjectController class

--- src/main/java/com/esprit/examen/controllers/ProjectController.java
+++ src/main/java/com/esprit/examen/controllers/ProjectController.java
@@ -1,12 +1,19 @@
 package com.esprit.examen.controllers;
 
+<<<<<<< HEAD
 import lombok.RequiredArgsConstructor;
+=======
+>>>>>>> 7e64d41 (add ProjectController class)
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin("*")
+<<<<<<< HEAD
 @RequiredArgsConstructor
 public class ProjectController {
 
+=======
+public class ProjectController {
+>>>>>>> 7e64d41 (add ProjectController class)
 }
